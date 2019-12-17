472d386957d01805eab56f9420413bcf01828364
IronTwist <47633722+IronTwist@users.noreply.github.com>
2019-12-17 10:05:10 +0200

    Update Main.java

--- ImplementareColectie/src/Main.java
+++ ImplementareColectie/src/Main.java
@@ -17,65 +17,80 @@ public class Main {
 		int bancnoteDe10 = 0;
 		int bancnoteDe5 = 0;
 		int bancnoteDe1 = 0;
+		int meniuOpt;
 		
 		ColectieImpl<Integer> portofel = new ColectieImpl<Integer>();
 		Iterator<Integer> it = portofel.iterator();
-		
-		do {
 			
-			adaugaBan = citesteTastatura("Adauga bani\n(1,5,10,50,100,500)(0 pentr a opri)\n in portofel: ");
-			if(adaugaBan != 0 && adaugaBan == 1 || adaugaBan == 5 || adaugaBan == 10 || adaugaBan == 50 || adaugaBan == 100 || adaugaBan == 500)
-				adaugaBancnote(portofel, adaugaBan);
-			else
-				System.out.println("Moneda invalida!");
-		}while(adaugaBan != 0);
+		do {
+			meniuOpt  = Meniu();
 		
-		while(it.hasNext()) {
-			 bancnota = it.next();
-			 switch(bancnota) {
-			 	case 1:
-			 		bancnoteDe1++;
-			 		break;
-			 	case 5:
-			 		bancnoteDe5++;
-			 		break;
-			 	case 10: 
-			 		bancnoteDe10++;
-			 		break;
-			 	case 50:
-			 		bancnoteDe50++;
-			 		break;
-			 	case 100:	
-			 		bancnoteDe100++;
-			 		break;
-			 	case 500:
-			 		bancnoteDe500++;
-			 		break;
-			 	default:
-			 		System.out.println("Eroare");
-			 }
-			 
-			 suma += bancnota;
-		}
+		switch(meniuOpt) {
+			case 1:
+				do {	
+				adaugaBan = citesteTastatura("\nAdauga bani\n(1,5,10,50,100,500)(0 pentr a opri)\n in portofel: ");
+					if(adaugaBan != 0 && adaugaBan == 1 || adaugaBan == 5 || adaugaBan == 10 || adaugaBan == 50 || adaugaBan == 100 || adaugaBan == 500){
+						adaugaBancnote(portofel, adaugaBan);
+					}else {
+						System.out.println("Moneda invalida!");
+					}
+				}while(adaugaBan != 0);
+				break;
+			case 2:
+				while(it.hasNext()) {
+					 bancnota = it.next();
+						 switch(bancnota) {
+						 	case 1:
+						 		bancnoteDe1++;
+						 		break;
+						 	case 5:
+						 		bancnoteDe5++;
+						 		break;
+						 	case 10: 
+						 		bancnoteDe10++;
+						 		break;
+						 	case 50:
+						 		bancnoteDe50++;
+						 		break;
+						 	case 100:	
+						 		bancnoteDe100++;
+						 		break;
+						 	case 500:
+						 		bancnoteDe500++;
+						 		break;
+						 	default:
+						 		System.out.println("Eroare");
+						 }
+					 
+					 suma += bancnota;
+				}
+				
+				System.out.println("Bancnote in portofel: " + portofel.dim());
+				System.out.println("Suma din portofel este: " + suma + " lei");
+				System.out.println("In portofel se afla " + bancnoteDe1 + " bancnote de 1 de lei.");
+				System.out.println("In portofel se afla " + bancnoteDe5 + " bancnote de 5 de lei.");
+				System.out.println("In portofel se afla " + bancnoteDe10 + " bancnote de 10 de lei.");
+				System.out.println("In portofel se afla " + bancnoteDe50 + " bancnote de 50 de lei.");
+				System.out.println("In portofel se afla " + bancnoteDe100 + " bancnote de 100 de lei.");
+				System.out.println("In portofel se afla " + bancnoteDe500 + " bancnote de 500 de lei.");
+				
+				break;
+			case 3: 
+				System.out.println("\nProgram termminat!");
+				break;
+			default:
+				System.out.println("Optiunea introdusa nu este valida!");
+		}//end switch opt
 		
-		System.out.println("Bancnote in portofel: " + portofel.dim());
-		System.out.println("Suma din portofel este: " + suma + " lei");
-		System.out.println("In portofel se afla " + bancnoteDe1 + " bancnote de 1 de lei.");
-		System.out.println("In portofel se afla " + bancnoteDe5 + " bancnote de 5 de lei.");
-		System.out.println("In portofel se afla " + bancnoteDe10 + " bancnote de 10 de lei.");
-		System.out.println("In portofel se afla " + bancnoteDe50 + " bancnote de 50 de lei.");
-		System.out.println("In portofel se afla " + bancnoteDe100 + " bancnote de 100 de lei.");
-		System.out.println("In portofel se afla " + bancnoteDe500 + " bancnote de 500 de lei.");
-		
-
-	}
+		}while(meniuOpt != 3);
+	}//end Main
 	
 	public static void  adaugaBancnote(ColectieImpl<Integer> col, int suma) {
 		col.adauga(suma);
 	}
 	
 	public static int citesteTastatura(String str) {
-		System.out.println(str);
+		System.out.print(str);
 		
 		try {
 			Scanner keyboard  = new Scanner(System.in);
@@ -86,6 +101,14 @@ public class Main {
 			return citesteTastatura(str);
 		}
 		
+	}
+	
+	public static int Meniu() {
+		System.out.println("\n1. Adauga bani in portofel");
+		System.out.println("2. Afiseaza portofel");
+		System.out.println("3. Exit");
+		int opt = citesteTastatura("Alege optiune: ");
+		return opt;
 	}
 	
 	
